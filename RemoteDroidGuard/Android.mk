b48573bef6a281daabff078e9a98007da98eb209
John Jacob <johnjacob@usa.com>
2021-12-29 21:01:13 -0500

    Changed LOCAL_MODULE_TAGS to eng (from optional) for Magisk and RemoteDroidGuard

--- RemoteDroidGuard/Android.mk
+++ RemoteDroidGuard/Android.mk
@@ -2,14 +2,14 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := privapp-permissions-org.microg.gms.droidguard.xml
-LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_TAGS := eng
 LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
 LOCAL_SRC_FILES := $(LOCAL_MODULE)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_TAGS := eng
 LOCAL_MODULE := RemoteDroidGuard
 LOCAL_SRC_FILES := org.microg.gms.droidguard-0.1.2-3.apk
 LOCAL_MODULE_CLASS := APPS
