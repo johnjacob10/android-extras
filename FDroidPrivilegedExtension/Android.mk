c523ba159b66a66bd3d8cea4d23bfda4b94ff90a
John Jacob <johnjacob@usa.com>
2022-01-20 14:26:15 -0500

    Added LOCAL_PRODUCT_MODULE back to privapp-permissions file.

--- FDroidPrivilegedExtension/Android.mk
+++ FDroidPrivilegedExtension/Android.mk
@@ -3,6 +3,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := privapp-permissions-org.fdroid.fdroid.privileged.xml
 LOCAL_MODULE_CLASS := ETC
+LOCAL_PRODUCT_MODULE := true
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
 LOCAL_SRC_FILES := $(LOCAL_MODULE)
