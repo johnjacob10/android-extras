0a3343bb228c984663eedeb9ed6dc5b5c0c41e8d
John Jacob <johnjacob@usa.com>
2025-01-23 20:50:00 -0500

    Update Android Auto to latest MindTheGapps 15.0 version.

--- AndroidAutoStub/Android.mk
+++ AndroidAutoStub/Android.mk
@@ -17,6 +17,7 @@ LOCAL_SRC_FILES := AndroidAutoStub.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_OPTIONAL_USES_LIBRARIES := androidx.window.extensions androidx.window.sidecar
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_REQUIRED_MODULES := privapp-permissions-com.google.android.projection.gearhead.xml
 include $(BUILD_PREBUILT)
