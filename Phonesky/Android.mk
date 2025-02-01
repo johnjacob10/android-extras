5ea348f694ac5803c084496653c054b359cfcdc6
John Jacob <johnjacob@usa.com>
2025-02-01 16:32:47 -0500

    Move to PRESIGNED with custom key for MicroG signature spoofing apps.

--- Phonesky/Android.mk
+++ Phonesky/Android.mk
@@ -18,6 +18,6 @@ LOCAL_MODULE_CLASS := APPS
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_OPTIONAL_USES_LIBRARIES := org.apache.http.legacy android.ext.adservices androidx.window.extensions androidx.window.sidecar
-LOCAL_CERTIFICATE := releasekey
+LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_REQUIRED_MODULES := privapp-permissions-com.android.vending.xml
 include $(BUILD_PREBUILT)
